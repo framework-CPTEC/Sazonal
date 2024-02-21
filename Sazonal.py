e8f8ccdc4dea5575524f20d45f487318f25162d7
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2024-02-21 14:59:37 -0300

    Create Sazonal.py

---
+++ Sazonal.py
@@ -0,0 +1,10 @@
+"""
+cptec-model - Biblioteca Python para a distibuição de Dados dos Modelos Numéricos.
+"""
+
+__version__ = "0.1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
