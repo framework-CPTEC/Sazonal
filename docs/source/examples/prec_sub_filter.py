7e6835ca4edcf925a81aa0c02a4dd0e4c817c5df
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2024-08-21 15:48:24 -0300

    Update prec_sub_filter.py

--- docs/source/examples/prec_sub_filter.py
+++ docs/source/examples/prec_sub_filter.py
@@ -1,33 +1,42 @@
-import subsaz.CPTEC_SUB as SUB
+import sazonal.CPTEC_SAZ as SAZ
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 # Inicializa o construtor
-sub = SUB.model()
-# Filtrar area Brasil
-sub.dict['area']['reduce'] = True 
-sub.dict['area']['minlat'] = -34.44
-sub.dict['area']['maxlat'] = -21.43
-sub.dict['area']['minlon'] = 301.14
-sub.dict['area']['maxlon'] = 320.57
+saz = SAZ.model()
+
+# Filtrar area definida
+saz.dict['area']['reduce'] = True
+saz.dict['area']['minlat'] = -34.44
+saz.dict['area']['maxlat'] = -21.43
+saz.dict['area']['minlon'] = 301.14
+saz.dict['area']['maxlon'] = 320.57
+
 # Requisição dos dados
-f = sub.load(date='20230104', var='prec', step='01', product='week' ,field='anomalies')
+f = saz.load(date='20240401', var='prec', product='mnth' ,field='anomalies')
+
 # Definir tamanho da figura
 fig = plt.figure(figsize=(10,8))
+
 # Setar figura unica
 ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
+
 # Colocar  Linhas de Borda dos paises e linhas costeiras
 ax.add_feature(cfeature.COASTLINE,color='grey')
 ax.add_feature(cfeature.BORDERS,color='grey')
+
 # Definir Regiao do Brasil
 ax.set_extent([-90,-30,10,-41], ccrs.PlateCarree())
+
 # Setar estados do Brasil
 states = cfeature.NaturalEarthFeature(category='cultural',
-                                            name='admin_1_states_provinces_lines',
-                                            scale='50m', facecolor='none')
+                                         name='admin_1_states_provinces_lines',
+                                         scale='50m', facecolor='none')
 # Colocar Estados Brasil
 ax.add_feature(states, edgecolor='gray')
+
 # Plotar variavel
-f.prec.plot()
+f.prec.sel(time="2024-04-01").plot()
 plt.show()
+
