d2c0d7dae458c055beeabd849596d54f8b927d75
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2024-08-21 15:44:19 -0300

    Update get_data_sub_oper.py

--- docs/source/examples/get_data_sub_oper.py
+++ docs/source/examples/get_data_sub_oper.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# Importa a ferramenta
-import cptecmodel.CPTEC_BAM as BAM
+# Importa a biblioteca
+import sazonal.CPTEC_SAZ as SAZ
 
 # Inicializa o construtor
-bam = BAM.model()
+saz = SAZ.model()
 
 # Data Condição Inicial (IC)
-date = '2022111800'
+date = '20240401'
 
-# variaveis
-vars = ['t2m', 'u', 'omega']
+# Variável
+var = ['prec']
 
-# Quantos passos previstos após inicialização do modelo
-steps = 5
+# Produto
+product = 'seas'
 
-# Niveis desejados (aplicado apenas as variaveis em niveis)
-levels = [1000, 850]
-
-# Para ativar a função de escrita em disco com uma copia da resição basta alterar a função save_netcdf para True
-bam.dict['save_netcdf'] = True
-
-# # Diretorio onde serão salvas as requisições
-# # Por padrão, quando não aterado, um diretorio chamado INPE será criado na pasta corrente do usuario
-
-# bam.dict['path_to_save'] = '/home/guatura/modelos'
+# Campo
+field = 'anomalies'
 
 # Requisição dos dados
-f = bam.load(date=date, var=vars, level=levels, steps=steps)
+f = saz.load(date=date, var=var, product=product ,field=field)
 
-# Um diretorio BAM será criado no diretorio corrente, no caso apresentado os dados serão salvos em:
-# INPE/BAM/TQ0666L064/brutos/hibrido/2022/11/18/00/GPOSNMC20220818002022081906P.fct.TQ0666L064.nc4
+# Imprimir o Xarray
+print(f)
 
 quit()
